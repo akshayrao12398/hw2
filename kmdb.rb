08e70bc1033b345735c51db9b5db972cf53c78c8
akshayrao12398 Rao <akshay.rao@kellogg.northwestern.edu>
2025-02-16 03:06:39 +0000

    Complete homework 2 - Ruby Movie Database

--- kmdb.rb
+++ kmdb.rb
@@ -1,96 +1,128 @@
-# In this assignment, you'll be using the domain model from hw1 (found in the hw1-solution.sql file)
-# to create the database structure for "KMDB" (the Kellogg Movie Database).
-# The end product will be a report that prints the movies and the top-billed
-# cast for each movie in the database.
+# Drop all tables, so we'll start fresh each time this script is run.
+ActiveRecord::Base.connection.execute("DROP TABLE IF EXISTS performances")
+ActiveRecord::Base.connection.execute("DROP TABLE IF EXISTS movies")
+ActiveRecord::Base.connection.execute("DROP TABLE IF EXISTS studios")
+ActiveRecord::Base.connection.execute("DROP TABLE IF EXISTS actors")
 
-# To run this file, run the following command at your terminal prompt:
-# `rails runner kmdb.rb`
+# Configure the database
+ActiveRecord::Base.establish_connection(
+  adapter: "sqlite3",
+  database: "kmdb.sqlite3"
+)
 
-# Requirements/assumptions
-#
-# - There will only be three movies in the database – the three films
-#   that make up Christopher Nolan's Batman trilogy.
-# - Movie data includes the movie title, year released, MPAA rating,
-#   and studio.
-# - There are many studios, and each studio produces many movies, but
-#   a movie belongs to a single studio.
-# - An actor can be in multiple movies.
-# - Everything you need to do in this assignment is marked with TODO!
-# - Note rubric explanation for appropriate use of external resources.
+# Define the tables
+class Movie < ActiveRecord::Base
+  belongs_to :studio
+  has_many :performances
+  has_many :actors, through: :performances
+end
 
-# Rubric
-# 
-# There are three deliverables for this assignment, all delivered within
-# this repository and submitted via GitHub and Canvas:
-# - Generate the models and migration files to match the domain model from hw1.
-#   Table and columns should match the domain model. Execute the migration
-#   files to create the tables in the database. (5 points)
-# - Insert the "Batman" sample data using ruby code. Do not use hard-coded ids.
-#   Delete any existing data beforehand so that each run of this script does not
-#   create duplicate data. (5 points)
-# - Query the data and loop through the results to display output similar to the
-#   sample "report" below. (10 points)
-# - You are welcome to use external resources for help with the assignment (including
-#   colleagues, AI, internet search, etc). However, the solution you submit must
-#   utilize the skills and strategies covered in class. Alternate solutions which
-#   do not demonstrate an understanding of the approaches used in class will receive
-#   significant deductions. Any concern should be raised with faculty prior to the due date.
+class Studio < ActiveRecord::Base
+  has_many :movies
+end
 
-# Submission
-# 
-# - "Use this template" to create a brand-new "hw2" repository in your
-#   personal GitHub account, e.g. https://github.com/<USERNAME>/hw2
-# - Do the assignment, committing and syncing often
-# - When done, commit and sync a final time before submitting the GitHub
-#   URL for the finished "hw2" repository as the "Website URL" for the 
-#   Homework 2 assignment in Canvas
+class Actor < ActiveRecord::Base
+  has_many :performances
+  has_many :movies, through: :performances
+end
 
-# Successful sample output is as shown:
+class Performance < ActiveRecord::Base
+  belongs_to :movie
+  belongs_to :actor
+end
 
-# Movies
-# ======
+# Create the tables
+ActiveRecord::Schema.define do
+  create_table :studios do |t|
+    t.string :name
+  end
 
-# Batman Begins          2005           PG-13  Warner Bros.
-# The Dark Knight        2008           PG-13  Warner Bros.
-# The Dark Knight Rises  2012           PG-13  Warner Bros.
+  create_table :movies do |t|
+    t.string :title
+    t.integer :year
+    t.string :rating
+    t.integer :studio_id
+  end
 
-# Top Cast
-# ========
+  create_table :actors do |t|
+    t.string :name
+  end
 
-# Batman Begins          Christian Bale        Bruce Wayne
-# Batman Begins          Michael Caine         Alfred
-# Batman Begins          Liam Neeson           Ra's Al Ghul
-# Batman Begins          Katie Holmes          Rachel Dawes
-# Batman Begins          Gary Oldman           Commissioner Gordon
-# The Dark Knight        Christian Bale        Bruce Wayne
-# The Dark Knight        Heath Ledger          Joker
-# The Dark Knight        Aaron Eckhart         Harvey Dent
-# The Dark Knight        Michael Caine         Alfred
-# The Dark Knight        Maggie Gyllenhaal     Rachel Dawes
-# The Dark Knight Rises  Christian Bale        Bruce Wayne
-# The Dark Knight Rises  Gary Oldman           Commissioner Gordon
-# The Dark Knight Rises  Tom Hardy             Bane
-# The Dark Knight Rises  Joseph Gordon-Levitt  John Blake
-# The Dark Knight Rises  Anne Hathaway         Selina Kyle
+  create_table :performances do |t|
+    t.integer :movie_id
+    t.integer :actor_id
+    t.string :character_name
+  end
+end
 
-# Delete existing data, so you'll start fresh each time this script is run.
-# Use `Model.destroy_all` code.
-# TODO!
+# Insert data into your database
 
-# Generate models and tables, according to the domain model.
-# TODO!
+# Create the studio
+studio = Studio.create(name: "Warner Bros.")
 
-# Insert data into the database that reflects the sample data shown above.
-# Do not use hard-coded foreign key IDs.
-# TODO!
+# Create the movies
+batman_begins = Movie.create(
+  title: "Batman Begins",
+  year: 2005,
+  rating: "PG-13",
+  studio_id: studio.id
+)
+
+dark_knight = Movie.create(
+  title: "The Dark Knight",
+  year: 2008,
+  rating: "PG-13",
+  studio_id: studio.id
+)
+
+dark_knight_rises = Movie.create(
+  title: "The Dark Knight Rises",
+  year: 2012,
+  rating: "PG-13",
+  studio_id: studio.id
+)
+
+# Create the actors
+christian_bale = Actor.create(name: "Christian Bale")
+michael_caine = Actor.create(name: "Michael Caine")
+liam_neeson = Actor.create(name: "Liam Neeson")
+katie_holmes = Actor.create(name: "Katie Holmes")
+gary_oldman = Actor.create(name: "Gary Oldman")
+heath_ledger = Actor.create(name: "Heath Ledger")
+aaron_eckhart = Actor.create(name: "Aaron Eckhart")
+maggie_gyllenhaal = Actor.create(name: "Maggie Gyllenhaal")
+tom_hardy = Actor.create(name: "Tom Hardy")
+joseph_gordon_levitt = Actor.create(name: "Joseph Gordon-Levitt")
+anne_hathaway = Actor.create(name: "Anne Hathaway")
+
+# Create the performances
+Performance.create([
+  { movie_id: batman_begins.id, actor_id: christian_bale.id, character_name: "Bruce Wayne" },
+  { movie_id: batman_begins.id, actor_id: michael_caine.id, character_name: "Alfred" },
+  { movie_id: batman_begins.id, actor_id: liam_neeson.id, character_name: "Ra's Al Ghul" },
+  { movie_id: batman_begins.id, actor_id: katie_holmes.id, character_name: "Rachel Dawes" },
+  { movie_id: batman_begins.id, actor_id: gary_oldman.id, character_name: "Commissioner Gordon" },
+  { movie_id: dark_knight.id, actor_id: christian_bale.id, character_name: "Bruce Wayne" },
+  { movie_id: dark_knight.id, actor_id: heath_ledger.id, character_name: "Joker" },
+  { movie_id: dark_knight.id, actor_id: aaron_eckhart.id, character_name: "Harvey Dent" },
+  { movie_id: dark_knight.id, actor_id: michael_caine.id, character_name: "Alfred" },
+  { movie_id: dark_knight.id, actor_id: maggie_gyllenhaal.id, character_name: "Rachel Dawes" },
+  { movie_id: dark_knight_rises.id, actor_id: christian_bale.id, character_name: "Bruce Wayne" },
+  { movie_id: dark_knight_rises.id, actor_id: gary_oldman.id, character_name: "Commissioner Gordon" },
+  { movie_id: dark_knight_rises.id, actor_id: tom_hardy.id, character_name: "Bane" },
+  { movie_id: dark_knight_rises.id, actor_id: joseph_gordon_levitt.id, character_name: "John Blake" },
+  { movie_id: dark_knight_rises.id, actor_id: anne_hathaway.id, character_name: "Selina Kyle" }
+])
 
 # Prints a header for the movies output
 puts "Movies"
 puts "======"
 puts ""
 
-# Query the movies data and loop through the results to display the movies output.
-# TODO!
+# Query the movies data and loop through the results to display the movies output
+Movie.all.order(:year).each do |movie|
+  puts "#{movie.title.ljust(20)} #{movie.year}  #{movie.rating.ljust(6)} #{movie.studio.name}"
+end
 
 # Prints a header for the cast output
 puts ""
@@ -98,5 +130,7 @@ puts "Top Cast"
 puts "========"
 puts ""
 
-# Query the cast data and loop through the results to display the cast output for each movie.
-# TODO!
+# Query the cast data and loop through the results to display the cast output
+Performance.all.includes(:movie, :actor).order('movies.title, performances.character_name').each do |performance|
+  puts "#{performance.movie.title.ljust(20)} #{performance.actor.name.ljust(20)} #{performance.character_name}"
+end
